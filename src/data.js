0aaedf8e3db41735cbe8e4a5710c3c57d3773005
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2022-06-01 11:37:01 +0530

    Created the data to fill in the cards for restaurant items

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const data = [
+    {
+        id: 1,
+        foodTitle: "Idli",
+        description: "",
+        price: 20
+    },
+    {
+        id: 2,
+        foodTitle: "Dosa",
+        description: "",
+        price: 30
+    },
+    {
+        id: 3,
+        foodTitle: "Biryani",
+        description: "",
+        price: 99
+    },
+    {
+        id: 4,
+        foodTitle: "Egg Chicken Roll",
+        description: "",
+        price: 50
+    },
+    {
+        id: 5,
+        foodTitle: "Puri",
+        description: "",
+        price: 30
+    },
+    {
+        id: 6,
+        foodTitle: "",
+        description: "",
+        price: 40
+    },
+    {
+        id: 7,
+        foodTitle: "Pizza",
+        description: "",
+        price: 120
+    },
+    {
+        id: 8,
+        foodTitle: "Veg Burger",
+        description: "",
+        price: 150
+    },
+    {
+        id: 9,
+        foodTitle: "Chicken Burger",
+        decscription: "",
+        price: 190,
+    },
+    {
+        id: 10,
+        foodTitle: "Samosa",
+        description: "",
+        price: 10
+    }
+];
