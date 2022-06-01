debea269ae6542150862570ec47318cbbd986144
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2022-06-01 13:19:47 +0530

    Edited the data.js

--- src/data.js
+++ src/data.js
@@ -2,61 +2,61 @@ export const data = [
     {
         id: 1,
         foodTitle: "Idli",
-        description: "",
+        description: "4 delicious idlis offered with Sambar, and Chutney.",
         price: 20
     },
     {
         id: 2,
         foodTitle: "Dosa",
-        description: "",
+        description: "A big butter Dosa on your plate ready to be served with Sambar, and Chutney",
         price: 30
     },
     {
         id: 3,
-        foodTitle: "Biryani",
-        description: "",
+        foodTitle: "Chicken Biryani",
+        description: "Do you even need a description for this?",
         price: 99
     },
     {
         id: 4,
         foodTitle: "Egg Chicken Roll",
-        description: "",
+        description: "Double egg, chicken roll. So tasty that it rolls your tummy with excitement!",
         price: 50
     },
     {
         id: 5,
         foodTitle: "Puri",
-        description: "",
+        description: "The classic Indian breakfast item. Each order contains 4 puris offered with green peas.",
         price: 30
     },
     {
         id: 6,
-        foodTitle: "",
-        description: "",
+        foodTitle: "Vada",
+        description: "The famous South Indian cuisine. 4 huge vadas, offered with green chutney.",
         price: 40
     },
     {
         id: 7,
         foodTitle: "Pizza",
-        description: "",
+        description: "Double cheese and macaroni pizza. Everyone's favourite.",
         price: 120
     },
     {
         id: 8,
         foodTitle: "Veg Burger",
-        description: "",
+        description: "A huge burger stuffed with all the green veggies that your mother advised you to eat when you were a child.",
         price: 150
     },
     {
         id: 9,
         foodTitle: "Chicken Burger",
-        decscription: "",
+        description: "Tired of eating green veggies? You deserve better.",
         price: 190,
     },
     {
         id: 10,
         foodTitle: "Samosa",
-        description: "",
+        description: "Samosa.... Who doesn't love it? 2 samosas along with green chutney included.",
         price: 10
     }
 ];
