5bfc901bef1cf2d111b1d6c3b28bd06bf72625a4
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2022-06-01 09:57:25 +0530

    Imported the NavbarComponent to the OrderPage

---
+++ src/pages/OrderPage.js
@@ -0,0 +1,12 @@
+import NavbarComponent from "../components/NavbarComponent";
+import CardComponent from "../components/CardComponent";
+
+function OrderPage() {
+    return (
+        <>
+            <NavbarComponent />
+        </>
+    );
+};
+
+export default OrderPage;
