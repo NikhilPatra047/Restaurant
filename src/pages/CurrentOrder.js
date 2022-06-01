ee2892af2f86979a5498a6236ffdda4d2d4aaea9
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2022-06-01 18:29:26 +0530

    Created the current order status page

---
+++ src/pages/CurrentOrder.js
@@ -0,0 +1,36 @@
+import { Card, Button } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+import NavbarComponent from "../components/NavbarComponent";
+import { Link } from "react-router-dom";
+
+function CurrentOrder() {
+
+    const state = useLocation();
+    console.log(state.state);
+
+    return (
+        <>
+            <NavbarComponent />
+            <div className="d-flex justify-content-center align-items-center" 
+            style={{margin: "0 auto", width: "80%", height: "90vh"}}>
+
+                <div>
+                    <Card style={{width: "100%"}}>
+                        <Card.Body>
+                            <Card.Title style={{textAlign: "center"}}>
+                                Do you want to confirm your order - <strong>{state.state}</strong> ?
+                            </Card.Title>
+                            <div style={{margin: "2em auto 0 auto", display: "flex", width: "80%", justifyContent: "space-around"}}>
+                                <Link to="/success"><Button variant="primary">Confirm Order</Button></Link>
+                                <Link to="/"><Button variant="danger">Cancel Order</Button></Link>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </div>
+
+            </div>
+        </>
+    );
+};
+
+export default CurrentOrder;
