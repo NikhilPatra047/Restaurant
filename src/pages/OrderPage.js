d4bc2fc307d8d2dc9549f7f24865b26f53d695c3
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2022-06-01 12:15:48 +0530

    Filled in the orderPage with cards representing a restaurant food item

--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -1,10 +1,23 @@
 import NavbarComponent from "../components/NavbarComponent";
 import CardComponent from "../components/CardComponent";
+import { data } from "../data";
 
 function OrderPage() {
     return (
         <>
             <NavbarComponent />
+            <div style={{display: "flex", flexDirection: "row", alignItems: "flex-end", justifyContent: "space-evenly", boxSizing: "border-box"}}>
+                <div className="row">
+                    {data.map((item) => {
+                        const { id, foodTitle, price } = item;
+                        return (
+                            <div className="col" style={{marginTop: "2em"}}>
+                                <CardComponent value={{ key: id, title: foodTitle, cost: price }} />
+                            </div>
+                        );
+                    })};
+                </div>
+            </div>
         </>
     );
 };
